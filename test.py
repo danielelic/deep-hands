3751c673a6ee954c3d562933aa9323142a60e0ee
Daniele Liciotti <danielelic@gmail.com>
2017-10-19 18:30:35 +0200

    [deep-hands] add test script for results

---
+++ test.py
@@ -0,0 +1,111 @@
+from __future__ import print_function
+
+from keras import backend as K
+
+from data import load_train_data, load_test_data
+from train_alexnet import getAlexNet
+from train_caffenet import getcaffenet
+from train_cnn import getCNN
+from train_cnn2 import getcnn2
+
+# input image dimensions
+img_rows, img_cols = 80, 80
+
+num_classes = 3
+channels = 3
+
+
+def precision(y_true, y_pred):
+    """Precision metric.
+
+    Only computes a batch-wise average of precision.
+
+    Computes the precision, a metric for multi-label classification of
+    how many selected items are relevant.
+    """
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+
+def recall(y_true, y_pred):
+    """Recall metric.
+
+    Only computes a batch-wise average of recall.
+
+    Computes the recall, a metric for multi-label classification of
+    how many relevant items are selected.
+    """
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+
+def f1score(y_true, y_pred):
+    def recall(y_true, y_pred):
+        """Recall metric.
+
+        Only computes a batch-wise average of recall.
+
+        Computes the recall, a metric for multi-label classification of
+        how many relevant items are selected.
+        """
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+        recall = true_positives / (possible_positives + K.epsilon())
+        return recall
+
+    def precision(y_true, y_pred):
+        """Precision metric.
+
+        Only computes a batch-wise average of precision.
+
+        Computes the precision, a metric for multi-label classification of
+        how many selected items are relevant.
+        """
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        precision = true_positives / (predicted_positives + K.epsilon())
+        return precision
+
+    precision = precision(y_true, y_pred)
+    recall = recall(y_true, y_pred)
+    return 2 * ((precision * recall) / (precision + recall))
+
+
+if __name__ == '__main__':
+    x_train, y_train, train_ids = load_train_data()
+    x_test, y_test, test_ids = load_test_data()
+
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, channels)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, channels)
+    input_shape = (img_rows, img_cols, channels)
+
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    x_train /= 255
+    x_test /= 255
+    print('x_train shape:', x_train.shape)
+    print('y_train shape:', y_train.shape)
+    print(x_train.shape[0], 'train samples')
+    print(x_test.shape[0], 'test samples')
+
+    modelsList = [(getCNN(), 'weights-cnn.h5'), (getcnn2(), 'weights-cnn2.h5'), (getAlexNet(), 'weights-alexnet.h5'),
+                  (getcaffenet(), 'weights-caffenet.h5')]
+
+    for idx, model in enumerate([el[0] for el in modelsList]):
+        print('-' * 30)
+        print('Loading model...')
+        print('-' * 30)
+
+        model.load_weights(modelsList[idx][1])
+
+        print('-' * 30)
+        print('Prediction results...')
+        print('-' * 30)
+
+        scores = model.evaluate(x_test, y_test, verbose=0)
+        for i, metric in enumerate(model.metrics_names):
+            print('%s: %f' % (metric, scores[i]))
